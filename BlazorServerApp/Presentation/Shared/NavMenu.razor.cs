49eccc63105e788cd14cf2eec4be15139f686a3f
Eduar Sanchez <eduar.sanchez@ceiba.com.co>
2024-10-31 09:01:03 -0500

    add  services mock

---
+++ BlazorServerApp/Presentation/Shared/NavMenu.razor.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entity;
+using Domain.Ports;
+using Microsoft.AspNetCore.Components;
+
+namespace Presentation.Shared;
+
+public partial class NavMenu : ComponentBase
+{
+    public ApplicationsEntity Application { get; set; }
+
+    [Inject]
+    public IApplicationService ApplicationService { get; set; }
+
+    public async Task GetApplicationsAsync()
+    {
+        Application = await ApplicationService.GetAplicacionesAsync();
+    }
+}
